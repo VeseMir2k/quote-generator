74469dab0c769a055d82b911c87a0f256287cd1c
VeseMir2k <vesemir2k@gmail.com>
2023-11-24 00:23:16 +0100

    manipulating dom with javascript

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-// let apiQuotes = [];
+const quoteContainer = document.querySelector("#quote-container");
+const quoteText = document.querySelector("#quote");
+const authorText = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const newQuoteBtn = document.querySelector("#new-quote");
+
+let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a arandom quote from apiQuotes array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  console.log(quote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.a) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.a;
+  }
+
+  // Check Quote length to determine styling
+  if (quote.q.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.q;
 }
 
 // Get Quotes From API
-// async function getQuotes() {
-//   const apiUrl = "https://type.fit/api/quotes";
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
+async function getQuotes() {
+  const apiUrl = "https://zenquotes.io/api/quotes";
 
-//     newQuote();
-//   } catch (error) {
-//     // Catch Error Here
-//   }
-// }
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
-// getQuotes();
-newQuote();
+getQuotes();
